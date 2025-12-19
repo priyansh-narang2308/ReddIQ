84a28642e2e39f0c1e7a914a430c5b02e9da7bd8
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-20 01:17:10 +0530

    feat: Tailwind added now proceeding with the form

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <h1 className="bg-red-500">Hello</h1>
+  </React.StrictMode>
 );
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      name: "ReddIQ",
+      description: "Chrome Extension",  
+    };
+  },
 });
